822564e62afb9b8d6bd6c3b4cfc9450d58bcb246
BuildTools <unconfigured@null.spigotmc.org>
2022-06-01 23:27:03 -0400

    TMPro, spinning obstacle test

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float torque;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 power = transform.up * torque * NumObj();
+        Debug.Log(power);
+
+        rb.AddTorque(transform.up * torque * NumObj(), ForceMode.Force);
+    }
+
+    private float NumObj() {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Object");
         
+        return Mathf.Pow(gos.Length, 0.25f);
     }
 }
