46f8117e17c5157bc5a3d4f8b7deb3b9ef1c9554
BuildTools <unconfigured@null.spigotmc.org>
2022-06-01 23:07:19 -0400

    Destruction Effect (particle spray)

--- Assets/Scripts/ObjectBehavior.cs
+++ Assets/Scripts/ObjectBehavior.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class ObjectBehavior : MonoBehaviour
 {
+    public GameObject deathEffect;
+
     private float lifeTime = 15.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+        StartCoroutine(Lifespan());
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DestroyObj")) {
+            KillObject();
+        }
+    }
+    private IEnumerator Lifespan()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        KillObject();
+    }
+    private void KillObject() {
+        GameObject go = Instantiate(deathEffect, transform.position, deathEffect.transform.rotation);
+        Destroy(go, 2);
+        Destroy(gameObject);
     }
 }
