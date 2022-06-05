bf5716e20851ced828669e6ac96ae64c5dce54fd
BuildTools <unconfigured@null.spigotmc.org>
2022-06-05 14:19:13 -0400

    Simple player movement with gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,48 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    private GameObject selected;
+    private CharacterController characterController;
+    public int movementSpeed;
+    private float velocity;
+    public float gravity = -9.81f;
+    private Vector3 camRotation;
+    public float sensitivity;
+
+    private Camera cam;
+
     public InputActionAsset actionAsset;
     private InputActionMap playerActionMap;
     private InputAction leftClick;
     private InputAction movement;
+    private InputAction sprint;
+    private InputAction mouse;
 
+    private GameObject selected;
     public TextMeshProUGUI selectText;
     public TextMeshProUGUI infoText;
 
     private Vector3 previousVel;
 
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+
     void Awake() {
         playerActionMap = actionAsset.FindActionMap("PlayerActionMap");
         leftClick = playerActionMap.FindAction("LeftClick");
         movement = playerActionMap.FindAction("Movement");
+        sprint = playerActionMap.FindAction("Sprint");
+        mouse = playerActionMap.FindAction("Mouse");
 
         leftClick.started += Select;
     }
 
     void Update()
     {
-        Vector2 input = movement.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
-
-        transform.Translate(move * Time.deltaTime * 10f);
+        MovePlayer();
+        MoveCamera();
     }
 
     void FixedUpdate()
@@ -50,8 +67,38 @@ public class PlayerController : MonoBehaviour
             selectText.gameObject.SetActive(false);
         }
     }
+
+    private void MovePlayer() {
+        Vector2 input = movement.ReadValue<Vector2>();
+        Vector3 horizontal = input.x * cam.transform.right;
+        Vector3 vertical = input.y * cam.transform.forward;
+        Vector3 move = (horizontal + vertical) * movementSpeed * Time.deltaTime; 
+
+        if (sprint.ReadValue<float>() == 1f) { move *= 1.75f; };
+
+        characterController.Move(move);
+
+        if (characterController.isGrounded) {
+            velocity = 0;
+        } else {
+            velocity += gravity * Time.deltaTime;
+            Vector3 upwardMove = new Vector3(0, velocity, 0);
+            characterController.Move(upwardMove);
+        }
+    }
+
+    private void MoveCamera() {
+        Vector2 mouseInput = mouse.ReadValue<Vector2>() * sensitivity;
+        camRotation.x = Mathf.Clamp(camRotation.x - mouseInput.y, -90f, 90f);
+        camRotation.y += mouseInput.x;
+
+        cam.transform.eulerAngles = camRotation;
+    }
+
     private void Select(InputAction.CallbackContext ctx) {
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Vector3 pos = new Vector3(Screen.width / 2, 0, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(pos);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
         
         if (Physics.Raycast(ray, out RaycastHit rayHit)) {
             if (rayHit.transform.CompareTag("Object")) {
